7d20134d8aebd18038c52ff010e223c4ff571360
Esteban Franco <42947441+efrancotec@users.noreply.github.com>
2024-07-28 21:54:07 -0600

    Create fileHelper class

---
+++ SwagLabsAutomation/Data/JSONData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using SwagLabsAutomation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwagLabsAutomation.Data
+{
+    public class JSONData : IData
+    {
+
+
+        public string GetUrl()
+        {
+            return getData("genera_info.json");
+        }
+
+        // PRIVATE METHODS
+        private string getData(string fileName)
+        {
+            var path = Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName + "/Data";
+            var info = new ConfigurationBuilder()
+                .SetBasePath(path)
+                .AddJsonFile("general_info.json")
+                .Build();
+
+            return info.GetSection(fileName).Value!;
+        }
+    }
+}
